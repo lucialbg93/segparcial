c294fb853ba7be6c15fdb168b3018b591f04508e
lucialbg93 <lucia.lbg93@gmail.com>
2023-06-08 01:02:58 -0300

    update requirements

--- segparcial/src/app.py
+++ segparcial/src/app.py
@@ -1,11 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route("/")
-def index():
-    return "Hola mundo"
+def index(): return "Hola mundo"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == "__main__":app.run()
 
